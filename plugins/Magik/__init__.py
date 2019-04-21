5f9c495624ee654215e49610c7ce662570417585
Riley Flynn <riley@rileyflynn.me>
2019-04-21 00:23:16 -0230

    Image

--- plugins/Magik/__init__.py
+++ plugins/Magik/__init__.py
@@ -3,7 +3,7 @@ import io
 import requests
 from discord import File
 from discord.ext import commands
-import wand
+from wand.image import Image
 
 from Plugin import BorobPlugin
 
@@ -15,7 +15,7 @@ class MagikPlugin(BorobPlugin):
     async def magik(self, ctx: commands.Context, image: str, scale: int = None):
         """Performs a magik destruction on an image."""
         resp = requests.get(image)
-        img = wand.image.Image(file=io.BytesIO(resp.content))
+        img = Image(file=io.BytesIO(resp.content))
         img.format = image.split("/")[-1].split(".")[-1]
         img.liquid_rescale(width=int(img.width * 0.5), height=int(img.height * 0.5), delta_x=int(0.5 * scale) if scale else 1, rigidity=0)
         processed = io.BytesIO()
